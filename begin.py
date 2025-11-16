2c2a0c2f0acaf690c76af3e4c0d65bad308dfdbd
NotaBot2077 <238048430+NotaBot2077@users.noreply.github.com>
2025-11-16 13:54:59 +0700

    Create begin.py

---
+++ begin.py
@@ -0,0 +1,16 @@
+a=9393.1321323232
+print('%.2f' % a)
+print('{:4f}'.format(a))
+a=False
+print(bool(a))
+a=0
+print(bool(a))
+a=1
+print(bool(a))
+a='fafnefj'
+print(bool(a))
+a=''
+print(bool(a))
+a=5+9j
+print(a.real)
+print(a.imag)
